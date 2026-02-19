d0fb256ba75bdcac82431c568b7f2330de17cd35
rpt521 <ri385049@ucf.edu>
2026-02-19 12:21:53 -0500

    Add comments for article page functionality
    Add comments to explain article page modifications.

--- script9.js
+++ script9.js
@@ -1,1 +1,10 @@
 //js to modify article page
+
+//When the user clicks the "Filter Articles" button, a menu appears with the checkboxes to filter. 
+
+
+//When the user clicks the "Add New Article" button, a form appears.
+
+//When the user checks/unchecks a box for filtering, each of the articles of that type are hidden/shown accordingly.
+
+//When the user enters a new article and presses "Add New Article", the content appears in the list with the correct styles.
